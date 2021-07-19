90108f1e74c7292976ffcb3d7f5b625159270dc4
wsjunior@gmail.com <wsjunior@gmail.com>
2021-07-19 19:47:56 -0300

    Início do Alurakut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,56 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSideBar(propriedades){
+  return (
+    <Box>
+    <img src={`https://github.com/${propriedades.githubUser}.png`} sytle={{ borderRadius: '8px' }} ></img>
+  </Box >
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = 'wsjunior1985';
+  const pessoas = ['juunegreiros', 'omariosouto', 'peas', 'rafaballerini', 'marcobrunodev', 'felipefialho']
+
+  return (
+  <>
+    <AlurakutMenu />
+    <MainGrid>
+      <div className="profileArea" sytle={{gridAarea: 'profileArea'}}>
+        <ProfileSideBar githubUser={githubUser} />
+      </div>
+      <div className="welcomeArea" sytle={{gridAarea: 'welcomeArea'}}>
+      <Box>
+        <h1 className="title">
+          Bem vindo
+        </h1>
+      <OrkutNostalgicIconSet></OrkutNostalgicIconSet>
+      </Box>
+      </div>
+      <div className="profileRelationsArea" sytle={{gridAarea: 'profileRelationsArea'}}>
+      <ProfileRelationsBoxWrapper>
+        <h2 className="smallTitle">Pessoas ({pessoas.length})</h2>
+        <ul>
+          {pessoas.map((item) => {
+            return (
+              <li>
+                <a href={`/users/${item}`} key={item}>
+                  <img src={`https://github.com/${item}.png`} />
+                  <span>{item}</span>
+                </a>
+              </li>
+            )
+            })}
+        </ul>
+      </ProfileRelationsBoxWrapper>
+      <Box>
+      Comunidades
+      </Box>
+      </div>
+    </MainGrid>
+  </>
+  )
 }
